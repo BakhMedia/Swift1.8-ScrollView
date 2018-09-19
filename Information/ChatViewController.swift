dfe710ae08d2f75002689c6f8634e819ddfdcbfe
Ilya Aleshin <>
2018-09-19 19:23:27 +0300

    init

---
+++ Information/ChatViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ChatViewController.swift
+//  ViewControllersAndLogs
+//
+//  Created by Ilya Aleshin on 21.06.2018.
+//  Copyright © 2018 Bakh. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
